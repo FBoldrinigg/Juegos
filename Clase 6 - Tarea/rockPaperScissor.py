c2dff18bc2854069c47f7c6ed8efecd9942553dd
FBoldrinigg <F.boldrini@alumno.um.edu.ar>
2019-04-29 18:44:39 -0300

    subiendo segundo ejercicio

---
+++ Clase 6 - Tarea/rockPaperScissor.py
@@ -0,0 +1,39 @@
+import random
+
+
+def rockPaperScissor():
+    result = ["rock", "paper", "scissor"]
+    scoreA = 0
+    scoreB = 0
+    while scoreA < 3 and  scoreB < 3:
+        playerIn = input("Choose one: 'rock', 'paper', 'scissor': ")
+        computerIn = result[random.randint(0, 2)]
+        if playerIn == computerIn:
+            print(playerIn, " x ", computerIn)
+            print("It's a tie.")
+        if playerIn == "rock" and computerIn == "paper":
+            print(playerIn, " x ", computerIn)
+            scoreB += 1
+        elif playerIn == "paper" and computerIn == "scissor":
+            print(playerIn, " x ", computerIn)
+            scoreB += 1
+        elif playerIn == "scissor" and computerIn == "rock":
+            print(playerIn, " x ", computerIn)
+            scoreB += 1
+        elif playerIn == "paper" and computerIn == "rock":
+            print(playerIn, " x ", computerIn)
+            scoreA += 1
+        elif playerIn == "rock" and computerIn == "scissor":
+            print(playerIn, " x ", computerIn)
+            scoreA += 1
+        elif playerIn == "scissor" and computerIn == "paper":
+            print(playerIn, " x ", computerIn)
+            scoreA += 1
+        print("The score is:", scoreA, " - ", scoreB)
+    if scoreB == 3:
+        print("You lost", scoreB, " - ", scoreA)
+    elif scoreA == 3:
+        print("You won", scoreA, " - ", scoreB)
+
+
+rockPaperScissor()
