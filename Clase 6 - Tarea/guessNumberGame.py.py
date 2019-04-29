d4d934336046d54dde4e75c39281ad474fabc2f7
FBoldrinigg <F.boldrini@alumno.um.edu.ar>
2019-04-29 17:24:40 -0300

    Subiendo el ejercicio de adivinar el numero

---
+++ Clase 6 - Tarea/guessNumberGame.py.py
@@ -0,0 +1,24 @@
+import random
+
+
+def guessGame():
+    randomNum = random.randint(1, 100)
+    tries = 10
+    while tries:
+        tries -= 1
+        guess = int(input("Take a guess between 1 and 100: "))
+        while guess < 1 or guess > 100:
+            print("Wrong input, ", guess, " is not a number between 1 and 100")
+            guess = int(input("Guess again: "))
+        if guess == randomNum:
+            print("Congratulations you guessed correctly in: ", abs(10 - tries), "tries.")
+            break
+        elif guess > randomNum:
+            print("The random number is smaller than ", guess)
+        else:
+            print("The random number is bigger than ", guess)
+
+    print("Game over. You ran out of tries, the number was: ", randomNum)
+
+
+guessGame()
